c5ad35f52d587aa067e3218a76f0ba6bef9552a3
HwangGoeun <hwang_gim@naver.com>
2022-01-08 01:27:30 +0900

    DEL Delete Button, config width

--- GUI/main.py
+++ GUI/main.py
@@ -27,22 +27,19 @@ def check(event) :
 window = Tk() # 윈도우 생성
 window.title("2022_Seoul_Hardware_Hackathon") # 윈도우 타이틀 설정
 window.geometry("500x300") # 윈도우 크기 설정
+window.resizable(False, False)
 
 items = [] # 리스트 박스 내의 내용이 들어 갈 공백 리스트 생성
 
 # 리스트 박스 생성
-listbox = Listbox(window, height = 0, selectmode = "extended") # Listbox 생성, selectmode에 대한 설명은 밑에 참고
+listbox = Listbox(window, height = 0, width = 300, selectmode = "extended") # Listbox 생성, selectmode에 대한 설명은 밑에 참고
 # 리스트 박스에 데이터 추가
 for i in range(len(items)): # 아이템 자료 개수만큼 반복하는데
     listbox.insert(END, itmes[i]) # 리스트 박스 끝에 자료 추가
 listbox.pack() # 리스트 박스 보이기
 
-buttonDel = Button(window, width = 10, text = "Delete", overrelief = "solid", command = delete)
-# 삭제 버튼 생성, overrelief는 마우스 커서를 위에 올렸을 때의 반응, solid는 검은 테두리 씌우기, 버튼 누르면 delete 함수 실행
-buttonDel.pack() # 삭제 버튼 보이기
-
 # Entry 생성
-entry = Entry(window, width = 30)
+entry = Entry(window, width = 300)
 entry.bind("<Return>", check) # enter 키가 입력되었을 때 add 함수 실행
 entry.pack() # entry 창 보이기
 
